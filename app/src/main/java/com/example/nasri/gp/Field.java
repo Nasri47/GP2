d8f8df612d38e12e0ec1f0cb0bf7476c5809dea4
Nasri <nasrelhadi1@gmail.com>
2018-05-27 17:58:12 +0300

    Second Screen

--- app/src/main/java/com/example/nasri/gp/Field.java
+++ app/src/main/java/com/example/nasri/gp/Field.java
@@ -3,13 +3,11 @@ package com.example.nasri.gp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class Field extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_field);
     }
-
 }
-
