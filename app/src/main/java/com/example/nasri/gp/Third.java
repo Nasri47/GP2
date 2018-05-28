bc255893aea9180c06a56e0dd2cd959354521979
Nasri <nasrelhadi1@gmail.com>
2018-05-28 23:43:00 +0300

    update Activities

---
+++ app/src/main/java/com/example/nasri/gp/Third.java
@@ -0,0 +1,13 @@
+package com.example.nasri.gp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Third extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
