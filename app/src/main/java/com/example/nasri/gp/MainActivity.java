9ed7de6b0a55084ac5dfd207dd46ecb13574fb07
Nasri <nasrelhadi1@gmail.com>
2018-07-08 12:08:43 +0300

    fix register layout scrollview

--- app/src/main/java/com/example/nasri/gp/MainActivity.java
+++ app/src/main/java/com/example/nasri/gp/MainActivity.java
@@ -1,31 +1,13 @@
 package com.example.nasri.gp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_third);
+        setContentView(R.layout.activity_main);
     }
-
-    public void onLogin(View view){
-        Intent log = new Intent(this , Main2Activity.class) ;
-        startActivity(log);
-    }
-
-    public void register(View view){
-        Intent log = new Intent(this , RegisterField.class) ;
-        startActivity(log);
-    }
-
-    public void fieldsList(View view){
-        Intent log = new Intent(this , FieldsList.class) ;
-        startActivity(log);
-    }
-
 }
--- app/src/main/java/com/example/nasri/gp/Third.java
+++ app/src/main/java/com/example/nasri/gp/Third.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class Third extends AppCompatActivity {
 
@@ -11,14 +12,40 @@ public class Third extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        Button mLoginButton = (Button) findViewById(R.id.btn_login);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent log = new Intent(Third.this, Main2Activity.class);
+                startActivity(log);
+            }
+        });
+
+        Button mRegisterButton = (Button) findViewById(R.id.btn_register);
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent log = new Intent(Third.this, RegisterField.class);
+                startActivity(log);
+            }
+        });
+
+        Button mReserveButton = (Button) findViewById(R.id.btn_reserve);
+        mReserveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent log = new Intent(Third.this, FieldsList.class);
+                startActivity(log);
+            }
+        });
     }
 
-    public void onLogin(View view){
-        Intent log = new Intent(this , Main2Activity.class) ;
-        startActivity(log);
-    }
-    public void register(View view){
-        Intent log = new Intent(this , RegisterField.class) ;
-        startActivity(log);
-    }
+//    public void onLogin(View view){
+//        Intent log = new Intent(this , Main2Activity.class) ;
+//        startActivity(log);
+//    }
+//    public void register(View view){
+//        Intent log = new Intent(this , RegisterField.class) ;
+//        startActivity(log);
+//    }
 }
